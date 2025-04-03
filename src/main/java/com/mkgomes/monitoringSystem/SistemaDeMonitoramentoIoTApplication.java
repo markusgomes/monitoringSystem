113f4a605e98f865aef7e5b57c4151b8dd97b189
markusgomes <contato.markus@gmail.com>
2025-04-03 10:07:01 -0300

    feat: projeto spring/spring project

---
+++ src/main/java/com/mkgomes/monitoringSystem/SistemaDeMonitoramentoIoTApplication.java
@@ -0,0 +1,13 @@
+package com.mkgomes.monitoringSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeMonitoramentoIoTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeMonitoramentoIoTApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mkgomes/monitoringSystem/SistemaDeMonitoramentoIoTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkgomes.monitoringSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeMonitoramentoIoTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
