97c657dfe773fc07f212b5db85312032412dd2d2
markusgomes <contato.markus@gmail.com>
2025-04-18 18:49:26 -0300

    fix: esp32 MAX9814 configuration

--- esp32_0.1/esp32_0.1.ino
+++ esp32_0.1/esp32_0.1.ino
@@ -1,32 +1,31 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <DHT.h>
+#include <math.h>
 
 //DHT22
-#define DHT_PIN 25
-#define DHT_INTERVIAL 30000
+const int DHT_PIN = 25;
+const unsigned long DHT_INTERVAL = 30000;
 #define DHTTYPE DHT22
 DHT dht(DHT_PIN, DHTTYPE);
-/*
-//MAX9814
-#define MAX_PIN 32
-#define SAMPLES 10
-float maxMaximas[SAMPLES];
-float maxMinimas[SAMPLES];
-#define SAMPLES_RMS 200
-#define SAMPLES_INTERVIAL 5000
-float rmsBuffer[SAMPLES_RMS];  // Buffer para armazenar todas as medições RMS
-#define DC_OFFSET 1.65         // Offset DC do MAX9814
-//#define GAIN_DB 60           // Ganho do módulo (60 dB)
-//#define MIC_SENSITIVITY -46  // Sensibilidade do microfone (ex: -46 dBV/Pa)
-//#define VREF_SPL 94          // 94 dB SPL = 1 Pa (referência padrão)
-*/
 
+//MAX9814
+const int MAX_PIN = 32;
+const int MAX_SAMPLES = 10;
+float maxMaximas[MAX_SAMPLES];
+float maxMinimas[MAX_SAMPLES];
+const unsigned long MAX_INTERVAL = 5000;
+const int SAMPLES_RMS = 200;
+const float DC_OFFSET = 1.65;  // Offset DC do MAX9814
+
+
+//CONF WIFI
 const char* redes[][2] = {
   { "Gnomos_Ext_2.4", "Edu@rd00" },
   { "Gnomos_2.4", "Edu@rd00" },
 };
 
+//CONF MQTT
 const char* mqtt_server = "192.168.15.24";
 const int mqtt_port = 1883;
 const char* mqtt_user = "servbd";
@@ -35,7 +34,7 @@ const char* mqtt_password = "Un1f3sp1";
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-//Função para conectar ao wifi
+//FUNÇÃO CONECTAR WIFI
 void connectToWiFi() {
   int numRedes = sizeof(redes) / sizeof(redes[0]);  // Calcula o número de redes (4)
 
@@ -51,8 +50,11 @@ void connectToWiFi() {
     }
 
     if (WiFi.status() == WL_CONNECTED) {
-      Serial.println("\nConectado!");
-      return;  // Sai da função se conectar
+      Serial.println("\nConectado com sucesso!");
+      Serial.println(redes[i][0]);
+      Serial.print("\nEndereço IP: ");
+      Serial.println(WiFi.localIP());
+      return;
     }
     Serial.println("\nFalha. Tentando próxima rede...");
   }
@@ -61,8 +63,8 @@ void connectToWiFi() {
   ESP.restart();
 }
 
-// Função para reconectar ao broker MQTT
-void reconnect() {
+//FUNÇÃO RECONECTAR BROKER MQTT
+void reconnectToBrokerMqtt() {
   while (!client.connected()) {
     Serial.print("Tentando conexão MQTT...");
     // Tenta conectar com client ID, usuário e senha
@@ -77,35 +79,46 @@ void reconnect() {
   }
 }
 
-void setup() {
-  Serial.begin(115200);  // Inicializa a comunicação serial
-  delay(1000);           // Pequena pausa para estabilizar a conexão serial
 
-  // Inicia a conexão Wi-Fi
+
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+
+  analogReadResolution(12);                    // ADC de 12 bits (0-4095)
+  analogSetPinAttenuation(MAX_PIN, ADC_11db);  // Atenuação para 3.3V
+
   WiFi.mode(WIFI_STA);
   connectToWiFi();
 
   client.setServer(mqtt_server, mqtt_port);
+
+  Serial.println("\nSISTEMA INICIANDO...");
 }
+
 
 void loop() {
 
   //MQTT
   if (!client.connected()) {
-    reconnect();
+    reconnectToBrokerMqtt();
   }
   client.loop();
 
+
   //DHT22
   static unsigned long lastDHT = 0;
-  if (millis() - lastDHT >= DHT_INTERVIAL) {
+  if (millis() - lastDHT >= DHT_INTERVAL) {
     lastDHT = millis();
+
     float temperatura = dht.readTemperature();
     float umidade = dht.readHumidity();
 
     if (isnan(temperatura) || isnan(umidade)) {
       Serial.println("[ERRO] Falha na leitura do DHT22. REINICIANDO...");
       dht.begin();
+      delay(2000);
       return;
     }
 
@@ -119,33 +132,46 @@ void loop() {
     }
   }
 
-  /*
-    //MAX9814
+  //MAX9814
   static unsigned long lastMaxUpdate = 0;
-  if (millis() - lastMaxUpdate >= SAMPLE_INTERVAL) {
+  if (millis() - lastMaxUpdate >= MAX_INTERVAL) {
     lastMaxUpdate = millis();
 
-    float sum_squares = 0;
-
-    for (int i = 0; i < SAMPLES_RMS; i++) {
-      float raw_voltage = analogReadMilliVolts(MAX_PIN) / 1000.0;  // Leitura em volts
-      float ac_signal = raw_voltage - DC_OFFSET;                   // Remove DC
-      sum_squares += ac_signal * ac_signal;
-      delayMicroseconds(100);  // Taxa de ~10 kHz
+    for (int i = 0; i < MAX_SAMPLES; i++) {
+      maxMaximas[i] = 0.0;
+      maxMinimas[i] = 150.0;
     }
 
-    float currentRms = sqrt(sum_squares / SAMPLES_RMS);
+    unsigned long startMillis = millis();
+    while (millis() - startMillis < MAX_INTERVAL) {
 
-    // Cálculo do dB SPL (considerando ganho e sensibilidade)
-    //float dB_SPL = 20 * log10(rms) + VREF_SPL - MIC_SENSITIVITY - GAIN_DB;
-    */
+      float sum_squares = 0;
+
+      for (int i = 0; i < SAMPLES_RMS; i++) {
+        float raw_voltage = analogReadMilliVolts(MAX_PIN) / 1000.0;  // Leitura em volts
+        float ac_signal = raw_voltage - DC_OFFSET;                   // Remove DC
+        sum_squares += ac_signal * ac_signal;
+        delayMicroseconds(100);
+      }
+
+      float currentRms = sqrt(sum_squares / SAMPLES_RMS);
+      float dB = 20 * log10(currentRms / 0.0001);  // Conversão para dB
+
+      updateRankings(dB);
+
+      delay(10);
+    }
+
+    publishMaxMin();
+  }
+
 
   // Exemplo: Publica uma mensagem a cada 10 segundos
   static unsigned long lastPublishTime = 0;
   if (millis() - lastPublishTime > 10000) {
     lastPublishTime = millis();
 
-    String mensagem = "Olá, MQTT!";
+    String mensagem = "Olá, MQTT Teste!";
     bool publicado = client.publish("topico_de_exemplo", mensagem.c_str());
 
     if (publicado) {
